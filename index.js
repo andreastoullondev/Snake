9373b68c98f6995f1940a6bccc128651cacb5adb
Andreas Toullon <andreasgitinit@gmail.com>
2025-08-18 22:37:29 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,10 @@
+window.onload = function(){
+
+    //Base para usar o canvas
+    canvas = document.getElementById("canvas");
+    ctx = canvas.getContext("2d");
+
+    //Variaveis
+
+    snake = [];
+}
